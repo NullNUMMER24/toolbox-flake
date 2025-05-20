1b58eacedb6ea3c034858599f044397afbe72f19
NullNUMMER24 <j.rohrbach@sensemail.ch>
2025-05-20 23:02:15 +0200

    remmina working

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A Nix flake providing a vanilla Remmina build and example of adding additional packages.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
