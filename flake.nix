3fb8ca881bc70b82cc8bb4f94ef9539573cc5002
NullNUMMER24 <j.rohrbach@sensemail.ch>
2025-05-20 22:53:34 +0200

    example flake

--- flake.nix
+++ flake.nix
@@ -1,21 +1,49 @@
 {
+  description = "A Nix flake providing a vanilla Remmina build and example of adding additional packages.";
+
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-        config.allowUnfree = true; # If you need proprietary packages
-      };
-    in {
-      packages = {
-        # List your packages here
-        firefox = pkgs.firefox;
-        vscode = pkgs.vscode;
-        default = pkgs.hello; # Default package
-      };
-    });
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        # Use the upstream Remmina without any plugins
+        remminaVanilla = pkgs.remmina;
+
+        # Example list of extra packages you might install
+        extraPackages = with pkgs; [
+          vim
+          htop
+          curl
+        ];
+      in {
+        packages = {
+          # Expose Remmina and extras as separate attributes
+          remmina = remminaVanilla;
+          extras = pkgs.stdenv.mkDerivation {
+            name = "extras";
+            buildInputs = extraPackages;
+            # no build step; this derivation just groups tools
+            unpackPhase = ''true'';
+            installPhase = ''mkdir -p $out && true'';
+          };
+        };
+
+        # Default package: Remmina
+        defaultPackage = remminaVanilla;
+
+        # Development shell including Remmina and any extras
+        devShells.default = pkgs.mkShell {
+          buildInputs = [ remminaVanilla ] ++ extraPackages;
+          shellHook = ''
+            echo "Remmina shell ready – version: $(remmina --version)"
+            echo "Extras installed: vim, htop, curl"
+          '';
+        };
+      }
+    );
 }
